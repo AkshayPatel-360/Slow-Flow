9c1472f3968de50fe993ea6e3789b930f31f5014
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-02-22 14:07:28 -0500

    Initial commit

---
+++ Assets/Resources/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float slowDownFactor = 0.05f;
+    public bool isSlowDown = false;
+
+    void DoSlowMotion()
+    {
+        Time.timeScale = slowDownFactor;
+    }
+}
---
+++ Assets/Resources/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float slowDownFactor = 0.05f;
+    public bool isSlowDown = false;
+
+    void DoSlowMotion()
+    {
+        Time.timeScale = slowDownFactor;
+    }
+}
